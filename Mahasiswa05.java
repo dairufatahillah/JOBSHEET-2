dff7d020dba0a7c7baff184eca19b05dcfb1cf47
Dairu Fatahillah <dairufatahillah60@gmail.com>
2026-02-26 20:07:57 +0700

    percobaan 3

--- Mahasiswa05.java
+++ Mahasiswa05.java
@@ -1,30 +1,39 @@
 public class Mahasiswa05 {
-    // Deklarasi Atribut
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
+    public String nama;
+    public String nim;
+    public String kelas;
+    public double ipk;
 
-    void tampilkanInformasi() {
+    public Mahasiswa05() {
+    }
+
+    public Mahasiswa05(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    public void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("IPK: " + ipk);
         System.out.println("Kelas: " + kelas);
     }
-
-    void ubahKelas(String kelasBaru) {
+    
+    public void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
-
-    void updateIpk(double ipkBaru) {
+    
+    public void updateIpk(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
             ipk = ipkBaru;
         } else {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
-
-    String nilaiKinerja() {
+    
+    public String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja sangat baik";
         } else if (ipk >= 3.0) {
